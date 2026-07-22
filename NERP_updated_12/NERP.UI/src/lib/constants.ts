d8ae0e9dde3d14c3cdd2c811eae7eaee99b99282
VISHWAS APPAJIGOWDA <Vishwas.ba@nexergroup.com>
2026-07-22 14:56:58 +0530

    removed Unnessary code

--- NERP_updated_12/NERP.UI/src/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-// Static branding used in a few places (Wall of Fame cards, Leaderboard).
-// Pulled out to one spot so it isn't duplicated/hardcoded per file.
-export const COMPANY_NAME = "Nexer Private Limited";
